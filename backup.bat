4e0aa46876e8d11b63e82882e47f4e0c299c3d2b
Adrian Lois <34519511+adrianlois@users.noreply.github.com>
2019-03-09 14:57:04 +0100

    Update backup.bat

--- backup.bat
+++ backup.bat
@@ -7,7 +7,7 @@ set ano=%date:~6,4%
 set mes=%date:~3,2%
 set dia=%date:~0,2%
 set hora=%time:~0,8%
-set backup=backup_%dia%-%mes%-%ano%.log
+set backuplog=backup_%dia%-%mes%-%ano%.log
 set backupzip=Backup_%dia%-%mes%-%ano%.zip
 
 :: Credenciales y Paths
@@ -24,18 +24,18 @@ set conexionFTP=ftp://%usuarioFTP%:%passwdFTP%@%servidorFTP%
 if exist "*backup*.log" ( del /F /Q "*backup*.log" )
 
 :: Mostrar fecha y hora del comienzo del proceso al princpio del log
-echo %dia%-%mes%-%ano% -- %hora% > %backup%
+echo %dia%-%mes%-%ano% -- %hora% > %backuplog%
 echo ----------------------
 
 :: Comprimir datos, generar log zip, agregarlo al backup log final y mostrar una línea de separación.
-7z a -tzip -p%passwd7z% -r %pathTempFichero7z% %pathLocalDatos% > zip%backup%
-type zip%backup% >> %backup%
-echo ######################################################################### >> %backup%
+7z a -tzip -p%passwd7z% -r %pathTempFichero7z% %pathLocalDatos% > zip%backuplog%
+type zip%backuplog% >> %backuplog%
+echo ######################################################################### >> %backuplog%
 
 :: Subir el fichero comprimido al servidor FTP, generar log FTP, añadirlo al log de backup y mostrar una línea de separación.
-winscp.com /log="ftp%backup%" /loglevel=2 /command "open %conexionFTP% -explicit" "cd %pathRemotoFTP%" "put %pathTempFichero7z%" "close" "exit"
-type ftp%backup% >> %backup%
-echo ######################################################################### >> %backup%
+winscp.com /log="ftp%backuplog%" /loglevel=2 /command "open %conexionFTP% -explicit" "cd %pathRemotoFTP%" "put %pathTempFichero7z%" "close" "exit"
+type ftp%backuplog% >> %backuplog%
+echo ######################################################################### >> %backuplog%
 
 :: Eliminar ficheros temporales: logs y fichero temporal backup zip.
 del /F /Q "zip*.log"
@@ -45,21 +45,21 @@ del /F /Q %pathTempFichero7z%
 :: Comprobar la eliminación de ficheros de log y fichero temporal backup zip, insertar el resultado en el log.
 :: Log de la compresión zip.
 if exist "zip*.log" (
-	echo -- zip%backup% no se eliminó correctamente >> %backup%
+	echo -- zip%backuplog% no se eliminó correctamente >> %backuplog%
 	) else (
-	   echo -- zip%backup% se eliminó correctamente >> %backup%
+	   echo -- zip%backuplog% se eliminó correctamente >> %backuplog%
 	)
 :: Log del envío de datos al servidor FTP.
 if exist "ftp*.log" (
-	echo -- ftp%backup% no se eliminó correctamente >> %backup%
+	echo -- ftp%backuplog% no se eliminó correctamente >> %backuplog%
 	) else (
-	   echo -- ftp%backup% se eliminó correctamente >> %backup%
+	   echo -- ftp%backuplog% se eliminó correctamente >> %backuplog%
 	)
 :: Fichero temporal backup zip
 if exist "D:\Backup*.zip" (
-	echo -- Backup.zip no se eliminó correctamente >> %backup%
+	echo -- Backup.zip no se eliminó correctamente >> %backuplog%
 	) else (
-	   echo -- Backup.zip se eliminó correctamente >> %backup%
+	   echo -- Backup.zip se eliminó correctamente >> %backuplog%
 	)
 
 :: Llamada al script Powershell para el envío del log vía Email.
